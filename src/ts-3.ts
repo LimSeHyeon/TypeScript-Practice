d45a60e34e71da4a3fb55a10f77b985b0824fe89
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-27 08:45:19 +0900

    20240527 인터페이스로 객체의 타입을 명시

---
+++ src/ts-3.ts
@@ -0,0 +1,14 @@
+interface User {
+    name: string;
+    age: number;
+    height?: number;
+}
+const user1: User = {
+    name: "Betty",
+    age: 30
+}
+const user2: User = {
+    name: "Betty",
+    age: 30,
+    height: 160
+}
