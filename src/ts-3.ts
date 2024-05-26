7f31834a7e0b85074af724a6ae31ed3369bfafc5
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-27 08:47:30 +0900

    20240527 readonly 속성은 변경 불가

--- src/ts-3.ts
+++ src/ts-3.ts
@@ -1,13 +1,18 @@
 interface User {
+    readonly id: number;
     name: string;
     age: number;
     height?: number;
 }
 const user1: User = {
+    id: 1,
     name: "Betty",
     age: 30
 }
+user1.name = "James";
+// user1.id = 2; // readonly 속성이므로 변경 불가
 const user2: User = {
+    id: 2,
     name: "Betty",
     age: 30,
     height: 160
