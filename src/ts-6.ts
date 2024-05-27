43cffbbe74785e59465122fe62e41cb33fb7b79e
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-27 09:21:13 +0900

    20240527 리터럴 -> 특정 Value만 허용

--- src/ts-6.ts
+++ src/ts-6.ts
@@ -5,23 +5,22 @@ id = "ABC";
 type IDType = string | number;
 
 let idList: IDType[];
-idList = [1, "A", 2, "B"]
-
+idList = [1, "A", 2, "B"];
 
 //Intersection Type
 interface Partner {
-    name: string;
-    contact: string;
+  name: string;
+  contact: string;
 }
 interface Contact {
-    email: string;
-    phone: string;
+  email: string;
+  phone: string;
 }
-type PartnerContact = Partner & Contact
+type PartnerContact = Partner & Contact;
 
-const partnerContact:PartnerContact = {
-    name: "Betty",
-    contact: "John",
-    email: "sample@naver.com",
-    phone: "010-1234-5678"
-}
+const partnerContact: PartnerContact = {
+  name: "Betty",
+  contact: "John",
+  email: "sample@naver.com",
+  phone: "010-1234-5678",
+};
