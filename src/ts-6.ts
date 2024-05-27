559b542ba72612443e9bdb3f82c52b60a72d32ed
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-27 09:02:34 +0900

    20240527 Intersection Type

--- src/ts-6.ts
+++ src/ts-6.ts
@@ -6,3 +6,22 @@ type IDType = string | number;
 
 let idList: IDType[];
 idList = [1, "A", 2, "B"]
+
+
+//Intersection Type
+interface Partner {
+    name: string;
+    contact: string;
+}
+interface Contact {
+    email: string;
+    phone: string;
+}
+type PartnerContact = Partner & Contact
+
+const partnerContact:PartnerContact = {
+    name: "Betty",
+    contact: "John",
+    email: "sample@naver.com",
+    phone: "010-1234-5678"
+}
